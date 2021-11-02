cd4788524494f348bfceee0b08a04c096ad222fa
Bruno Roberto Gomes <bruno@brgomes.com>
2021-11-02 14:47:49 -0300

    Initial commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Store;
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*DB::table('users')->insert(
+            [
+                'name' => 'Administrator',
+                'email' => 'admin@admin.com',
+                'email_verified_at' => now(),
+                'password' => '123',
+                'remember_token' => 'abcdefg',
+            ]
+        );*/
+
+        factory(User::class, 40)->create()->each(function ($user) {
+            $user->store()->save(factory(Store::class)->make());
+        });
+    }
+}
