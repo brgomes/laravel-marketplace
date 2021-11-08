854c15bc7817c459f5aa7973b55a81580d67e63c
= <bruno@brgomes.com>
2021-11-08 11:08:19 -0300

    Add products to cart

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/product/{slug}', 'HomeController@single')->name('product.single');
 
+Route::prefix('cart')->name('cart.')->group(function () {
+    Route::get('/', 'CartController@index')->name('index');
+    Route::post('add', 'CartController@add')->name('add');
+});
+
 Route::group(['middleware' => 'auth'], function () {
     Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function () {
         /*Route::prefix('stores')->name('stores.')->group(function () {
