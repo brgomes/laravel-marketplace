4bd9cb1569ebb3f324f8f53b5e459dfe695e2a89
= <bruno@brgomes.com>
2021-11-08 10:54:07 -0300

    Product single

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/product/{slug}', 'HomeController@single')->name('product.single');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function () {
