527a98abdbc72911cab947b9953b3e99e046377b
Bruno Roberto Gomes <bruno@brgomes.com>
2021-11-12 09:59:18 -0300

    Update migrations

--- database/migrations/2021_11_02_151035_create_category_product_table.php
+++ database/migrations/2021_11_02_151035_create_category_product_table.php
@@ -17,8 +17,8 @@ class CreateCategoryProductTable extends Migration
             $table->unsignedBigInteger('product_id');
             $table->unsignedBigInteger('category_id');
 
-            $table->foreign('product_id')->references('id')->on('products');
-            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('CASCADE');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('CASCADE');
         });
     }
 
--- database/migrations/2021_11_05_174635_create_product_photos_table.php
+++ database/migrations/2021_11_05_174635_create_product_photos_table.php
@@ -19,7 +19,7 @@ class CreateProductPhotosTable extends Migration
             $table->string('image');
             $table->timestamps();
 
-            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('CASCADE');
         });
     }
 
